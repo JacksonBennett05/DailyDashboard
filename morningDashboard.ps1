71918fe4cac147df8b2b17b239551d02676a1c08
Jackson Bennett <147541969+JacksonBennett05@users.noreply.github.com>
2025-11-12 13:07:34 -0500

    Skeleton Code

---
+++ morningDashboard.ps1
@@ -0,0 +1,6 @@
+# MorningDashboard.ps1
+$time = Get-Date -Format "dddd, MMM dd yyyy - hh:mm tt"
+
+Write-Host "========== GOOD MORNING =========="
+Write-Host "Date: $time"
+Write-Host "-----------------------------------"
