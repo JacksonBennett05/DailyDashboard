96fbd9b09239459c2605ae7f6de0e6c8d0a66ba8
JacksonBennett05 <jcb263@gmail.com>
2026-01-29 15:48:03 -0500

    Weather + Sunrise/Sunset

--- morningDashboard.ps1
+++ morningDashboard.ps1
@@ -1,6 +1,17 @@
-# MorningDashboard.ps1
-$time = Get-Date -Format "dddd, MMM dd yyyy - hh:mm tt"
+$time = Get-Date -Format "dddd, MMM dd yyyy"
+$lat = 38.9896
+$lng = -76.9371
+$name = "Jackson"
+$weather = Invoke-WebRequest "https://wttr.in/20740?format=%C+%t"
+$response = Invoke-RestMethod "https://api.sunrise-sunset.org/json?lat=$lat&lng=$lng&tzid=America/New_York"
+$sunrise = $response.results.sunrise
+$sunset  = $response.results.sunset
 
-Write-Host "========== GOOD MORNING =========="
-Write-Host "Date: $time"
+
+
+Write-Host "========== Good Morning $name =========="
+Write-Host "Todays Date is: $time"
+Write-Host "The weather in College Park is $($weather.Content)"
+Write-Host "Sunrise will be at $sunrise"
+Write-Host "Sunset will be at $sunset"
 Write-Host "-----------------------------------"
